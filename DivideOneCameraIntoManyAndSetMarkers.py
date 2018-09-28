79908226cb50d4d25bf1346002744d6d4aa44d37
Martynas Žiemys <32711544+Martynas-Ziemys@users.noreply.github.com>
2018-09-28 15:18:10 +0300

    Update DivideOneCameraIntoManyAndSetMarkers.py

--- DivideOneCameraIntoManyAndSetMarkers.py
+++ DivideOneCameraIntoManyAndSetMarkers.py
@@ -10,20 +10,20 @@ data.lens = data.lens * n
 data.draw_size = data.draw_size / n
 data.shift_x = data.shift_x * n - (n/2 - 0.5)
 data.shift_y = (data.shift_y/aspect * n - (n/2 - 0.5))*aspect
-marker = bpy.context.scene.timeline_markers.new(bpy.context.object.name,  frame=bpy.context.scene.frame_current)
+marker = bpy.context.scene.timeline_markers.new('',  frame=bpy.context.scene.frame_current)
 marker.camera = bpy.context.object
 bpy.context.scene.frame_current += 1
 direction = 1 
 for i in range(0, n):
     for a in range(1, n):
         bpy.ops.object.duplicate()
-        marker = bpy.context.scene.timeline_markers.new(bpy.context.object.name,  frame=bpy.context.scene.frame_current)
+        marker = bpy.context.scene.timeline_markers.new('',  frame=bpy.context.scene.frame_current)
         marker.camera = bpy.context.object
         bpy.context.scene.frame_current += 1
         bpy.context.object.data.shift_x += direction
     if i<n-1:
         bpy.ops.object.duplicate()
-        marker = bpy.context.scene.timeline_markers.new(bpy.context.object.name,  frame=bpy.context.scene.frame_current)
+        marker = bpy.context.scene.timeline_markers.new('',  frame=bpy.context.scene.frame_current)
         marker.camera = bpy.context.object
         bpy.context.scene.frame_current += 1
         bpy.context.object.data.shift_y += aspect
